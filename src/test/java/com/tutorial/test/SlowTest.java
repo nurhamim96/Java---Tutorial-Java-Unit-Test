88456fce93071644e7d8afb0ee4cf6e8eb7eac09
ibad nurhamim <nurhamimibad96@gmail.com>
2021-01-14 16:22:28 +0700

    [TUTORIAL] Eksekusi test secara parallel

--- src/test/java/com/tutorial/test/SlowTest.java
+++ src/test/java/com/tutorial/test/SlowTest.java
@@ -2,15 +2,36 @@ package com.tutorial.test;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
 
 import java.util.concurrent.TimeUnit;
 
+@Execution(ExecutionMode.CONCURRENT)
 public class SlowTest {
+
+    // Jika tidak dijalankan secara parallel ketiga test dibawah akan memakan waktu 12 detik.
+    // Sedangkan kalo dijalankan secara parallel ketiga test dibawah hanya memakan waktu 4 detik saja.
 
     @Test
     @Timeout(value = 5, unit = TimeUnit.SECONDS)
-    void testSlow() throws InterruptedException {
-        Thread.sleep(10_000); // test nya akan gagal karena lebih dari 5 detik
+    void testSlow1() throws InterruptedException {
+//        Thread.sleep(10_000); // test nya akan gagal karena lebih dari 5 detik
+        Thread.sleep(4_000);
+    }
+
+    @Test
+    @Timeout(value = 5, unit = TimeUnit.SECONDS)
+    void testSlow2() throws InterruptedException {
+//        Thread.sleep(10_000); // test nya akan gagal karena lebih dari 5 detik
+        Thread.sleep(4_000);
+    }
+
+    @Test
+    @Timeout(value = 5, unit = TimeUnit.SECONDS)
+    void testSlow3() throws InterruptedException {
+//        Thread.sleep(10_000); // test nya akan gagal karena lebih dari 5 detik
+        Thread.sleep(4_000);
     }
 }
 
